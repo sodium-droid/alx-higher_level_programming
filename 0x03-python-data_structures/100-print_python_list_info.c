59944b47065e3c516bcfb4bc347635fb4952cb20
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2024-01-09 12:33:03 -0800

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -15,5 +15,5 @@ void print_python_list_info(PyObject *p)
     printf("[*] size of the Python List = %li\n", size);
     printf("[*] Allocated = %li\n", obj->allocated);
     for (i = 0; i < size; i++)
-        printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+        printf("Element %i: %s\n", i, Py_TYPE(obj)->tp_name);
 }
