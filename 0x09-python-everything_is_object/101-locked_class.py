1f008f0f1f1360fb2293ad2ac7a6d011acd382aa
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2024-02-02 05:34:39 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,8 +8,20 @@ dynmaically created attributes
 
 
 class LockedClass:
+    """
+
+    LockedClass with no class or object attribute, that prevents
+    user from dynamically creating new instance attributes
+    except if the new instance attribute is called first_name
+
+    """
     __slots__ = ["first_name"]
 
     def __init__(self, first_name):
-        """Method to initialize objects"""
+        """
+        Method to initialize objects
+        Args:
+            first_name: name of Individual (Object)
+
+        """
         self.first_name = first_name
